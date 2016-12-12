ec35946a755c27b944f1c5b2f812b3899b2f6eb5
galafit <galafit@gmail.com>
2016-12-12 20:17:02 +0300

    addon to previous commit

---
+++ src/com/biorecorder/edflib/SignalConfig.java
@@ -0,0 +1,106 @@
+package com.biorecorder.edflib;
+
+/**
+ *
+ */
+public class SignalConfig {
+    private  int numberOfSamplesInEachDataRecord;
+    private String prefiltering = "None";
+    private String transducerType = "Unknown";
+    private String label = "";
+    private int digitalMin = -8388608;
+    private int digitalMax = 8388607;
+    private double physicalMin = -8388608;
+    private double physicalMax = 8388607;
+    private String physicalDimension = "";  // uV or Ohm
+
+
+    public int getDigitalMin() {
+        return digitalMin;
+    }
+
+    public int getDigitalMax() {
+        return digitalMax;
+    }
+
+    public double getPhysicalMin() {
+        return physicalMin;
+    }
+
+    public double getPhysicalMax() {
+        return physicalMax;
+    }
+
+    public String getPhysicalDimension() {
+        return physicalDimension;
+    }
+
+    public int getNumberOfSamplesInEachDataRecord() {
+        return numberOfSamplesInEachDataRecord;
+    }
+
+    public void setDigitalMin(int digitalMin) {
+        this.digitalMin = digitalMin;
+    }
+
+    public void setDigitalMax(int digitalMax) {
+        this.digitalMax = digitalMax;
+    }
+
+    public void setPhysicalMin(double physicalMin) {
+        this.physicalMin = physicalMin;
+    }
+
+    public void setPhysicalMax(double physicalMax) {
+        this.physicalMax = physicalMax;
+    }
+
+    public void setPhysicalDimension(String physicalDimension) {
+        this.physicalDimension = physicalDimension;
+    }
+
+
+    public void setNumberOfSamplesInEachDataRecord(int numberOfSamplesInEachDataRecord) {
+        this.numberOfSamplesInEachDataRecord = numberOfSamplesInEachDataRecord;
+    }
+
+    public String getPrefiltering() {
+        return prefiltering;
+    }
+
+    public void setPrefiltering(String prefiltering) {
+        this.prefiltering = prefiltering;
+    }
+
+    public String getTransducerType() {
+        return transducerType;
+    }
+
+    public void setTransducerType(String transducerType) {
+        this.transducerType = transducerType;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public SignalConfig copy() {
+        SignalConfig resultingSignalConfig = new SignalConfig();
+        resultingSignalConfig.setDigitalMax(getDigitalMax());
+        resultingSignalConfig.setDigitalMin(getDigitalMin());
+        resultingSignalConfig.setPhysicalMax(getPhysicalMax());
+        resultingSignalConfig.setPhysicalMin(getPhysicalMin());
+        resultingSignalConfig.setLabel(getLabel());
+        resultingSignalConfig.setPrefiltering(getPrefiltering());
+        resultingSignalConfig.setTransducerType(getTransducerType());
+        resultingSignalConfig.setPhysicalDimension(getPhysicalDimension());
+        resultingSignalConfig.setNumberOfSamplesInEachDataRecord(getNumberOfSamplesInEachDataRecord());
+        return resultingSignalConfig;
+    }
+
+
+}
